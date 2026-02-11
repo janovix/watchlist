6a55ad524de5c8a6aa349f4e0014f214f1583546
azaelvaldo <azael_valdo@hotmail.com>
2026-02-11 13:32:02 -0600

    fix(middleware): update matcher to exclude 'monitoring' from route handling

--- src/middleware.ts
+++ src/middleware.ts
@@ -231,6 +231,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
 	matcher: [
-		"/((?!api|_next/static|_next/image|favicon.ico|site.webmanifest|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+		"/((?!api|monitoring|_next/static|_next/image|favicon.ico|site.webmanifest|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
 	],
 };
